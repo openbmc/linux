3daa7f921737aabe065aafa5490fb267ea2e574a
Adi Gangidi <adi.gangidi@rackspace.com>
2016-03-17 10:01:10 -0500

    Updated SCU88 register and SCU90 init values
    Before This change:
    a) SCU90[0]=1, function pin Incorrectly defined. It must be pull down internally.
    b) For SCU88, bits 7:0 were set to 1. That is: We were reading : PWMx or VPIGx instead of GPIONx (GPIONx gives us the PCIe inventory status, where x is bit number). Because of this PCIe inventory was showing up wrong.
    
    After This Change:
    a) SCU90[0]=0 
    b) Bits (7:0) of SCU 88 are set to 0 . (According to Page 111 of data sheet these have to be set to 0 for us to to read GPION0 to GPIO N7 which indicate if PCIe device is present ). 
    
    Description of pins 0 of SCU 90:
    Enable SD1 Function Pin 
    
    Description of pins 7:0 of SCU 88:
    7 RW Enable PWM7 or VPIG7 function pin  (SCU90[5:4]=0x2 select Video pin)
    6 RW Enable PWM6 or VPIG6 function pin   (SCU90[5:4]=0x2 select Video pin)
    5 RW Enable PWM5 or VPIG5 function pin    (SCU90[5:4]!=0 select Video pin)
    4 RW Enable PWM4 or VPIG4 function pin     (SCU90[5:4]!=0 select Video pin)
    3 RW Enable PWM3 or VPIG3 function pin      (SCU90[5:4]!=0 select Video pin)
    2 RW Enable PWM2 or VPIG2 function pin        (SCU90[5:4]!=0 select Video pin)
    1 RW Enable PWM1 or VPIG1 function pin        (SCU90[5:4]=0x3 select Video pin)
    0 RW Enable PWM0 or VPIG0 function pin         (SCU90[5:4]=0x3 select Video pin)

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -118,8 +118,8 @@ static void __init do_common_setup(void)
 	/* SCU setup */
 	writel(0x01C000FF, AST_IO(AST_BASE_SCU | 0x88));
 	writel(0xC1C000FF, AST_IO(AST_BASE_SCU | 0x8c));
-	writel(0x01C0007F, AST_IO(AST_BASE_SCU | 0x88));
-	writel(0x003FA009, AST_IO(AST_BASE_SCU | 0x90));
+	writel(0x01C00000, AST_IO(AST_BASE_SCU | 0x88));    
+	writel(0x003FA008, AST_IO(AST_BASE_SCU | 0x90));
 
 	/* Setup scratch registers */
 	writel(0x00000042, AST_IO(AST_BASE_LPC | 0x170));
